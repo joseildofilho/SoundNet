fe3f7c5be37c68b95873271cd3844cfe0733e88e
joseildo filho <joseildofilho@yahoo.com.br>
2018-09-02 21:07:15 -0300

    sme changes

--- main.py
+++ main.py
@@ -1,109 +1,16 @@
-from abc import ABC, abstractmethod
-from threading import Thread
-from _thread import start_new_thread
+''' Sound Net'''
+
+from physical_layer import Physical
 
 class Data:
     def __init__(self):
         self.header = ""
         self.data = ""
-class Layer(ABC, Thread):
-    '''Abstract class for Layer'''
 
-    def __init__(self):
-        Thread.__init__(self)
-        self._gate_top = None
-        self._gate_down = None
-        self._data = None
-    
-    @property
-    def data(self):
-        return self._data
 
-    @data.getter
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, data):
-        self._data = data
-
-    @abstractmethod
-    def send_down(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def send_up(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def receive_up(self, data):
-        raise NotImplementedError
-
-    @abstractmethod
-    def receive_down(self, data):
-        raise NotImplementedError
-
-    def set_gate(self, top=None, down=None):
-        ''' gaters setter'''
-        self._gate_top = top
-        self._gate_down = down
-    
-    @abstractmethod
-    def process_data(self):
-        raise NotImplementedError
-    
-    @abstractmethod
-    def run(self):
-        raise NotImplementedError
-
-class Gate():
-
-    def __init__(self, layer_top, layer_down):
-        self._top = layer_top
-        self._top.set_gate(down=self)
-
-        self._down = layer_down
-        self._down.set_gate(top=self)
-
-    def move_down(self, data):
-        self._down.receive_top(data)
-
-    def move_top(self, data):
-        self._top.receive_down(data)
-
-class Physical(Layer):
-    '''Physical Layer'''
-    def __init__(self, channel):
-        Layer.__init__(self)
-
-        self._channel = channel
-
-    def send_down(self):
-        self._gate_down.move_down(self._data)
-
-    def send_up(self):
-        self._gate_top.move_up(self._data)
-
-    def receive_up(self, data):
-        self._data = data
-
-    def receive_down(self, data):
-        self.data = data
-
-    def process_data(self):
-        raise NotImplementedError
-
-    def run(self):
-        start_new_thread(self._listen, ('Physical Listener', 1))
-        start_new_thread(self._speak, ('Physical Speaker', 1))
-        while True:
-            pass
-    def _listen(self, thread_name, delay):
-        raise NotImplementedError
-
-    def _speak(self, thread_name, delay):
-        raise NotImplementedError
 if '__main__' == __name__:
+    #listener = Listener(1000,44100)
+    #print(listener.listen())
     layer = Physical()
     layer.data = Data()
     layer.start()
--- physical_layer.py
+++ physical_layer.py
@@ -2,9 +2,9 @@ from asyncio import Queue
 from threading import Thread, Lock, Semaphore
 
 import pyaudio
-from numpy import fromstring, float64, int16, zeros, float32, pi, arange, sin
+from numpy import fromstring, float64, int16, int32, zeros, float32, pi, arange, sin, int64, log10, array
 from numpy.random import rand
-from numpy.fft import rfft, irfft
+from numpy.fft import rfft, irfft, fft
 
 import time
 
@@ -13,19 +13,27 @@ from wave_visualizer import WaveVisualizer
 
 import soundcard as sc
 
+from multiprocessing.pool import ThreadPool
+
 LISTENER = sc.default_microphone()
+
+calculate_channel = lambda max_frequency, bit_len: lambda channel: int((bit_len*channel/max_frequency) - 1)
 
 class Physical(Layer):
     '''Physical Layer'''
-    def __init__(self, channel):
+    def __init__(self):
         Layer.__init__(self)
 
-        self._channel = channel
         self._size_message = 44100
-        self._speaker = Speaker(self._size_message, self._size_message)
-        self._speaker.start()
-        self._listener = Listener(self._size_message, self._size_message, channels=[self._size_message//4])
+
+        self._speaker = Speaker(self._size_message, 44100)
+
+        self._listener = Listener(self._size_message//2, 44100)
+
         self._listener.start()
+        time.sleep(3)
+        self._speaker.start()
+
         self._shower = WaveVisualizer('listen', lim=1)
         # self._shower.start()
         self._queue_data = Queue()
@@ -45,24 +53,38 @@ class Physical(Layer):
         raise NotImplementedError
 
     def run(self):
-        z = zeros(self._size_message//2 + 1)
-        z[self._size_message//2] = 100000000
-        z = irfft(z)
-        print(z.shape)
+        z = zeros(self._size_message)
+        self._queue_data.put_nowait('1')
         while True:
-            # x = rfft(self._listener.listen())
-            # x /= abs(x).max()
-            # #self._shower.show(x)
-            # if x[self._size_message//2] > 0.7:
-            #    print('1')
-            #    time.sleep(0.5)
-            self._speaker.speak(z)
+            time.sleep(1)
+            if not self._queue_data.empty():
+                aux = self._queue_data.get_nowait()
+                self._speaker.speak(0)
+                time.sleep(5)
+                for bit in list(aux):
+                    if int(bit) == 1:
+                        self._speaker.speak(1)
+                self._speaker.speak(2)
+                self._queue_data.task_done()
             break
 
 
 class Speaker(Thread):
-    def __init__(self, size_message, frame_rate):
+    def __init__(self, size_message, frame_rate, channels=3, channel_min=1000, channel_max = 9000):
         Thread.__init__(self)
+
+        chan_calc = calculate_channel(channel_max, size_message)
+        self._channels = [0 for _ in range(channels)]
+        step = (channel_max - channel_min) // (channels - 1)
+
+        aux = zeros(size_message)
+
+        duration = 1.0
+
+        for i in range(channels):
+            f = channel_min + step * i
+            self._channels[i] = (sin(2 * pi * arange(frame_rate * duration) * f / frame_rate)).astype(float32)
+        print(self._channels)
         self._size_message = size_message
         self._frame_rate = frame_rate
         self._paudio = pyaudio.PyAudio()
@@ -75,45 +97,67 @@ class Speaker(Thread):
         self._in = Semaphore(1)
         self._out = Semaphore(0)
         self.data = None
-    def speak(self, data):
+
+
+    def speak(self,channel):
         self._in.acquire()
-        self._data = data
+        time.sleep(1)
+        self._data = self._channels[channel]
         self._out.release()
     def run(self):
         fill = chr(0)*self._size_message*2
         while True:
-            while not self._out.acquire(blocking=False):
-                #print('filling in')
-                self._stream.write(fill)
-            print('speaking')
+            self._out.acquire()
+            self._stream.write(fill)
             self._stream.write(self._data)
             self._in.release()
 
-
 class Listener(Thread):
-    def __init__(self, size_message, frame_rate, threshold=0.3, channels = []):
+    def __init__(self, size_message, frame_rate, threshold=0.7, channels=3, channel_min=1000, channel_max=11000):
         Thread.__init__(self)
         self._size_message = size_message
         self._frame_rate = frame_rate
         self._threshold = threshold
-        self._channels = channels
+        self._channels = zeros(channels,dtype=int64)
+        chan_calc = calculate_channel(channel_max, size_message)
+        step = channel_max - channel_min
+        for i in range(channels):
+            self._channels[i] = chan_calc(channel_min + step * i)
+        self._channels = array([500,2500,4500])
         self._paudio = pyaudio.PyAudio()
-        self._stream = self._paudio.open(format=pyaudio.paInt16,
+        self._stream = self._paudio.open(format=pyaudio.paInt32,
                                         channels=1,
                                         rate=frame_rate,
                                         input=True,
                                         frames_per_buffer=size_message)
         self._stream.start_stream()
+        self._pool = ThreadPool(100)
+        self._queuen_in = Queue()
+
     def listen(self):
-        return fromstring(self._stream.read(self._size_message), dtype=int16)
+        return fromstring(self._stream.read(self._size_message), dtype=int32)
+
     def run(self):
-        w = WaveVisualizer('a')
-        w.start()
+        code = int64(0)
         while True:
-            x = rfft(self.listen()).astype(float64)
-            x /= abs(x).max()
-            print(x[1000:].argmax())
-            for i in abs(x[self._channels]) > self._threshold:
-                print('bit', i)
+            x = self.listen()
+            print('\r' + str(x.max()), end='')
+            if x.max() > 2000000000:
+                self._pool.apply_async(self._decode(x, code))
+                code += 1
 
+    def _decode(self, msg, code):
+        print('decoding',code)
+        msg = rfft(msg)
+        msg /= abs(msg[100:]).max()
+        print(msg.argmax())
+        for i in abs(msg[self._channels]) > self._threshold:
+            if i:
+                print('bit', i, abs(msg[self._channels]) > self._threshold, 'code', code, msg.argmax())
+                break
 
+class Decoder(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self._queue = Queue()
+        self._lock = Lock()
